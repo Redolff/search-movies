f4c9c789f66cd1e908e4d32fd83bb476cd484cae
Federico Redolfo <redolfofederico@gmail.com>
2024-07-25 00:20:46 -0300

    Add debounce for no repeat movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
+import { useCallback } from 'react'
 import { ListOfMovies } from './Components/ListOfMovies'
 import { useMovies } from './hooks/useMovies'
 import { useSearch } from './hooks/useSearch'
+import debounce from 'just-debounce-it'
 
 export const App = () => {
     const { search, setSearch, error } = useSearch()
     const { movies, getMovies, loading } = useMovies({ search })
-
+    
+    // Evita que la busqueda se haga continuamente al escribir
+    const debouncedGetMovies = useCallback(
+        debounce(search => {
+            console.log('search', search)
+            getMovies({ search })
+        }, 300), 
+    [getMovies])
+        
     const handleSubmit = (e) => {
         e.preventDefault()
         getMovies({ search })
@@ -14,7 +24,8 @@ export const App = () => {
 
     const handleChange = (e) => {
         const newSearch = e.target.value
-        getMovies({ search: newSearch })
+        setSearch(newSearch)
+        debouncedGetMovies(newSearch)
     }
 
     return (
